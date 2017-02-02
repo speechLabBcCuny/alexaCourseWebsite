3b2f480c8da4d38a29b0c908fc96d55b70db1106
Michael Mandel <mim@mr-pc.org>
2017-02-02 14:43:56 -0500

    added test cases to commandline version

--- parseCourseWebsite.py
+++ parseCourseWebsite.py
@@ -2,9 +2,24 @@
 
 import cisc1600status
 
-def main():
-    print cisc1600status.parseCisc1600Page()
+import datetime
 
+def main():
+    now = datetime.datetime.now()
+    
+    print "Default time"
+    print cisc1600status.parseCisc1600Page()
+    print
+    print "Now"
+    print cisc1600status.parseCisc1600Page(now)
+    print
+    print "1 day from now"
+    print cisc1600status.parseCisc1600Page(now + datetime.timedelta(days=1))
+    print
+    print "5 days from now"
+    print cisc1600status.parseCisc1600Page(now + datetime.timedelta(days=5))
+    
+    
 
 if __name__ == '__main__':
     main()
